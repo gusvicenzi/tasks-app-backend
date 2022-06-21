942ba32f39153ab3dca2838c47dc8b5ec5821988
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2022-06-21 12:15:26 -0300

    Upload initial files

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  semi: false,
+  printWidth: 80,
+  parser: 'flow',
+  endOfLine: 'crlf',
+  singleAttributePerLine: true,
+}
+
