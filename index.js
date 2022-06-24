5f513c7c7924ad8293c7af927edbeaa7b4494865
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2022-06-24 11:40:41 -0300

    Finish db configuration

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
+const db = require('./config/db') //Access db through knex
+const consign = require('consign') //Consign helps loading all modules
 
-app.get('/', (req, res) => {
-  res.status(200).send('Meu Backend!')
+// Passes app to all modules loaded from consign
+consign().then('./config/middlewares.js').into(app)
+
+app.db = db // app.db access db using knex
+
+app.listen(3000, () => {
+  console.log('Backend executando... ')
 })
